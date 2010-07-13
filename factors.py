bb1c74fc9abf0571b02e5142a018cacac272f673
alex <alex@alex-desktop.(none)>
2010-07-12 21:57:43 -0400

    Made faster factorization method

--- factors.py
+++ factors.py
@@ -1,10 +1,27 @@
 from math import sqrt
+from primes import generate_primes_up_to
 
-def factorize(num):
-    """Returns a list of factors"""
-    factors = []
-    for i in range(1,int(sqrt(num)+1)):
-        if num % i == 0:
-            factors.append(i)
-            factors.append(int(num/i))
-    return sorted(list(set(factors)))
+def factorize(n, pr=None):
+        """Return a set of factors of n.
+
+        factorize(int[, list]) -> set
+
+        pr is an optional sorted list of primes up to sqrt(n), and will be generated if it is not given"""
+        if pr == None:
+            pr = generate_primes_up_to(int(sqrt(n)))
+        orig = n
+        facs = set()
+        while n > 1:
+            added = False
+            for p in pr:
+                if n % p == 0:
+                    newfacs = set(x * p for x in facs)
+                    n = int(n/p)
+                    facs = facs.union(newfacs)
+                    facs.add(p)
+                    added = True
+                    break
+            if not added:
+                facs.add(n)
+                break
+        return facs.union({1,orig})
